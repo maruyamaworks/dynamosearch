7388c32941338c7222735a0a5d174a61fce8a7ce
Kenichi Maruyama <11133876+maruyamaworks@users.noreply.github.com>
2025-11-09 22:47:51 +0900

    test: add test scripts

--- packages/dynamosearch/src/index.test.ts
+++ packages/dynamosearch/src/index.test.ts
@@ -1,5 +1,5 @@
 import { test, expect, beforeAll } from 'vitest';
-import { DynamoDBClient, BatchWriteItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, BatchWriteItemCommand, ScanCommand } from '@aws-sdk/client-dynamodb';
 import type { DynamoDBStreamEvent } from 'aws-lambda';
 import StandardAnalyzer from './analyzers/StandardAnalyzer.js';
 import DynamoSearch from './index.js';
@@ -15,7 +15,7 @@ beforeAll(async () => {
   await dynamosearch.createIndexTable();
 });
 
-test('processRecords', async () => {
+test('processRecords (INSERT)', async () => {
   const event: DynamoDBStreamEvent = {
     Records: [
       {
@@ -38,52 +38,6 @@ test('processRecords', async () => {
         },
         eventSourceARN: 'stream-ARN',
       },
-      /*
-      {
-        eventID: '2',
-        eventName: 'MODIFY',
-        eventVersion: '1.0',
-        eventSource: 'aws:dynamodb',
-        awsRegion: 'us-east-1',
-        dynamodb: {
-          Keys: {
-            Id: { N: '101' },
-          },
-          NewImage: {
-            Message: { S: 'This item has changed' },
-            Id: { N: '101' },
-          },
-          OldImage: {
-            Message: { S: 'New item!' },
-            Id: { N: '101' },
-          },
-          SequenceNumber: '222',
-          SizeBytes: 59,
-          StreamViewType: 'NEW_AND_OLD_IMAGES',
-        },
-        eventSourceARN: 'stream-ARN',
-      },
-      {
-        eventID: '3',
-        eventName: 'REMOVE',
-        eventVersion: '1.0',
-        eventSource: 'aws:dynamodb',
-        awsRegion: 'us-east-1',
-        dynamodb: {
-          Keys: {
-            Id: { N: '101' },
-          },
-          OldImage: {
-            Message: { S: 'This item has changed' },
-            Id: { N: '101' },
-          },
-          SequenceNumber: '333',
-          SizeBytes: 38,
-          StreamViewType: 'NEW_AND_OLD_IMAGES',
-        },
-        eventSourceARN: 'stream-ARN',
-      },
-      */
     ],
   };
   const analyzer = await StandardAnalyzer.getInstance();
@@ -93,6 +47,34 @@ test('processRecords', async () => {
     keys: [{ name: 'Id', type: 'HASH' }],
   });
   await dynamosearch.processRecords(event.Records);
+
+  const client = new DynamoDBClient({
+    endpoint: 'http://localhost:8000',
+  });
+  const { Items } = await client.send(new ScanCommand({
+    TableName: 'dynamosearch_test',
+  }));
+  expect(Items).toHaveLength(3);
+  expect(Items).toEqual(expect.arrayContaining([
+    {
+      p: { S: '_' },
+      s: { B: new Uint8Array([0]) },
+      dc: { N: '1' },
+      'tc:Message': { N: '2' },
+    },
+    {
+      p: { S: 'Message;new' },
+      s: { B: new Uint8Array([0, 1, 0, 0, 0, 2, 232, 244, 177, 186, 163, 88, 89, 159]) },
+      k: { S: 'N101' },
+      h: { B: new Uint8Array([232]) },
+    },
+    {
+      p: { S: 'Message;item!' },
+      s: { B: new Uint8Array([0, 1, 0, 0, 0, 2, 232, 244, 177, 186, 163, 88, 89, 159]) },
+      k: { S: 'N101' },
+      h: { B: new Uint8Array([232]) },
+    },
+  ]));
 });
 
 test('search', async () => {
@@ -150,4 +132,132 @@ test('search', async () => {
       score: expect.closeTo(0.575),
     },
   ]);
+});
+
+test('processRecords (MODIFY)', async () => {
+  const event: DynamoDBStreamEvent = {
+    Records: [
+      {
+        eventID: '2',
+        eventName: 'MODIFY',
+        eventVersion: '1.0',
+        eventSource: 'aws:dynamodb',
+        awsRegion: 'us-east-1',
+        dynamodb: {
+          Keys: {
+            Id: { N: '101' },
+          },
+          NewImage: {
+            Message: { S: 'This item has changed' },
+            Id: { N: '101' },
+          },
+          OldImage: {
+            Message: { S: 'New item!' },
+            Id: { N: '101' },
+          },
+          SequenceNumber: '222',
+          SizeBytes: 59,
+          StreamViewType: 'NEW_AND_OLD_IMAGES',
+        },
+        eventSourceARN: 'stream-ARN',
+      },
+    ],
+  };
+  const analyzer = await StandardAnalyzer.getInstance();
+  const dynamosearch = new DynamoSearch({
+    indexTableName: 'dynamosearch_test',
+    attributes: [{ name: 'Message', analyzer }],
+    keys: [{ name: 'Id', type: 'HASH' }],
+  });
+  await dynamosearch.processRecords(event.Records);
+
+  const client = new DynamoDBClient({
+    endpoint: 'http://localhost:8000',
+  });
+  const { Items } = await client.send(new ScanCommand({
+    TableName: 'dynamosearch_test',
+  }));
+  expect(Items).toHaveLength(5);
+  expect(Items).toEqual(expect.arrayContaining([
+    {
+      p: { S: '_' },
+      s: { B: new Uint8Array([0]) },
+      dc: { N: '1' },
+      'tc:Message': { N: '4' },
+    },
+    {
+      p: { S: 'Message;this' },
+      s: { B: new Uint8Array([0, 1, 0, 0, 0, 4, 232, 244, 177, 186, 163, 88, 89, 159]) },
+      k: { S: 'N101' },
+      h: { B: new Uint8Array([232]) },
+    },
+    {
+      p: { S: 'Message;item' },
+      s: { B: new Uint8Array([0, 1, 0, 0, 0, 4, 232, 244, 177, 186, 163, 88, 89, 159]) },
+      k: { S: 'N101' },
+      h: { B: new Uint8Array([232]) },
+    },
+    {
+      p: { S: 'Message;has' },
+      s: { B: new Uint8Array([0, 1, 0, 0, 0, 4, 232, 244, 177, 186, 163, 88, 89, 159]) },
+      k: { S: 'N101' },
+      h: { B: new Uint8Array([232]) },
+    },
+    {
+      p: { S: 'Message;changed' },
+      s: { B: new Uint8Array([0, 1, 0, 0, 0, 4, 232, 244, 177, 186, 163, 88, 89, 159]) },
+      k: { S: 'N101' },
+      h: { B: new Uint8Array([232]) },
+    },
+  ]));
+});
+
+test('processRecords (REMOVE)', async () => {
+  const event: DynamoDBStreamEvent = {
+    Records: [
+      {
+        eventID: '3',
+        eventName: 'REMOVE',
+        eventVersion: '1.0',
+        eventSource: 'aws:dynamodb',
+        awsRegion: 'us-east-1',
+        dynamodb: {
+          Keys: {
+            Id: { N: '101' },
+          },
+          OldImage: {
+            Message: { S: 'This item has changed' },
+            Id: { N: '101' },
+          },
+          SequenceNumber: '333',
+          SizeBytes: 38,
+          StreamViewType: 'NEW_AND_OLD_IMAGES',
+        },
+        eventSourceARN: 'stream-ARN',
+      },
+    ],
+  };
+  const analyzer = await StandardAnalyzer.getInstance();
+  const dynamosearch = new DynamoSearch({
+    indexTableName: 'dynamosearch_test',
+    attributes: [{ name: 'Message', analyzer }],
+    keys: [{ name: 'Id', type: 'HASH' }],
+  });
+  await dynamosearch.processRecords(event.Records);
+
+  const client = new DynamoDBClient({
+    endpoint: 'http://localhost:8000',
+  });
+  const { Items } = await client.send(new ScanCommand({
+    TableName: 'dynamosearch_test',
+  }));
+  expect(Items).toHaveLength(1);
+  expect(Items).toEqual(expect.arrayContaining([
+    {
+      p: { S: '_' },
+      s: { B: new Uint8Array([0]) },
+      dc: { N: '0' },
+      'tc:Message': { N: '0' },
+    },
+  ]));
 });
