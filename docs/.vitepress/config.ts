c08f26b4448f0b5c06cea7abd80709781f723e76
Kenichi Maruyama <11133876+maruyamaworks@users.noreply.github.com>
2025-11-14 03:19:21 +0900

    docs: update base path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+  base: '/dynamosearch/',
   title: 'DynamoSearch',
   description: 'Full-text search by DynamoDB, for DynamoDB',
   themeConfig: {
